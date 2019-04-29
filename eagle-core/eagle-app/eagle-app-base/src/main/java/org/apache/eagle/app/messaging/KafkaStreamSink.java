227cf9f4315b2f26aba8e20ce853f65a18025519
mazhiyong <wingmazhiyong@gmail.com>
2019-04-29 14:00:02 +0800

    debug kafka producer old api

--- eagle-core/eagle-app/eagle-app-base/src/main/java/org/apache/eagle/app/messaging/KafkaStreamSink.java
+++ eagle-core/eagle-app/eagle-app-base/src/main/java/org/apache/eagle/app/messaging/KafkaStreamSink.java
@@ -16,18 +16,21 @@
  */
 package org.apache.eagle.app.messaging;
 
-import org.apache.kafka.clients.producer.ProducerRecord;
+import kafka.producer.KeyedMessage;
+import kafka.producer.Producer;
+import kafka.producer.ProducerConfig;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 //import kafka.javaapi.producer.Producer;
 //import kafka.producer.KeyedMessage;
 //import kafka.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.KafkaProducer;
+//import org.apache.kafka.clients.producer.ProducerConfig;
+//import org.apache.kafka.clients.producer.Producer;
+//import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.collection.Seq;
 
 import java.util.Map;
 import java.util.Properties;
@@ -51,19 +54,24 @@ public class KafkaStreamSink extends StormStreamSink<KafkaStreamSinkConfig> {
         Properties properties = new Properties();
         properties.put("metadata.broker.list", config.getBrokerList());
         properties.put("serializer.class", config.getSerializerClass());
-        //properties.put("key.serializer.class", config.getKeySerializerClass());
+        properties.put("key.serializer.class", config.getKeySerializerClass());
 
+        /* org.apache.kafka.clients.producer api
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, config.getKeySerializerClass());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, config.getValueSerializerClass());
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getBrokerList());
+        */
 
         // new added properties for async producer
         properties.put("producer.type", config.getProducerType());
         properties.put("batch.num.messages", config.getNumBatchMessages());
         properties.put("request.required.acks", config.getRequestRequiredAcks());
         properties.put("queue.buffering.max.ms", config.getMaxQueueBufferMs());
-        //ProducerConfig producerConfig = new ProducerConfig(properties);
-        producer = new KafkaProducer(properties);
+
+        ProducerConfig producerConfig = new ProducerConfig(properties);
+        producer = new Producer(producerConfig);
+        // org.apache.kafka.clients.producer api
+        //producer = new KafkaProducer(properties);
     }
 
     @Override
@@ -71,9 +79,11 @@ public class KafkaStreamSink extends StormStreamSink<KafkaStreamSinkConfig> {
         try {
             String output = new ObjectMapper().writeValueAsString(event);
             // partition key may cause data skew
-            //producer.send(new KeyedMessage(this.topicId, key, output));
+            producer.send((Seq<KeyedMessage>) new KeyedMessage(this.topicId, key, output));
             LOG.info("test topicId={} msg={}",this.topicId, output);
-            producer.send(new ProducerRecord(this.topicId, output));
+            // org.apache.kafka.clients.producer api
+            //producer.send(new ProducerRecord(this.topicId, output));
+
             //producer.send(new KeyedMessage(this.topicId, output));
         } catch (Exception ex) {
             LOG.error(ex.getMessage(), ex);
