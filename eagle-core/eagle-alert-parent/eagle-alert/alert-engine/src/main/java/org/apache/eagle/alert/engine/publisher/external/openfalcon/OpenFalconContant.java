17370e81242456cc55e9a0fafb02324eb6bb6f61
mazhiyong <wingmazhiyong@gmail.com>
2019-05-09 16:50:46 +0800

    debug

--- eagle-core/eagle-alert-parent/eagle-alert/alert-engine/src/main/java/org/apache/eagle/alert/engine/publisher/external/openfalcon/OpenFalconContant.java
+++ eagle-core/eagle-alert-parent/eagle-alert/alert-engine/src/main/java/org/apache/eagle/alert/engine/publisher/external/openfalcon/OpenFalconContant.java
@@ -1,9 +1,9 @@
 package org.apache.eagle.alert.engine.publisher.external.openfalcon;
 
 public class OpenFalconContant {
-    public static final String OPEN_FALCONF_CONFIG_ROOT = "application";
-    public static final String OPEN_FALCON_SERVER_URL = "openFalcon.serverUrl";
-    public static final String OPEN_FALCON_ENDPOINT = "openFalcon.endpoint";
+    public static final String OPEN_FALCONF_CONFIG_ROOT = "application.openFalcon";
+    public static final String OPEN_FALCON_SERVER_URL = "serverUrl";
+    public static final String OPEN_FALCON_ENDPOINT = "endpoint";
 
     public static final int OPEN_FALCON_STEP_DEFAULT_VALUE = 60;
     public static final int OPEN_FALCON_VALUE_DEFAULT_VALUE = 9;
