87d4937dcaad2d27adb0e32c5075c11c20f05df4
mazhiyong <wingmazhiyong@gmail.com>
2019-06-10 10:29:05 +0800

    add drelephant webapp

--- eagle-enterprise/eagle-enterprise-standard/src/main/java/org/apache/eagle/enterprise/standard/storm/EnteriseStandardParserBolt.java
+++ eagle-enterprise/eagle-enterprise-standard/src/main/java/org/apache/eagle/enterprise/standard/storm/EnteriseStandardParserBolt.java
@@ -6,10 +6,15 @@ import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Tuple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class EnteriseStandardParserBolt extends BaseRichBolt {
+    private static Logger LOG = LoggerFactory.getLogger(EnteriseStandardParserBolt.class);
+    private static final String DATASOURCE_TIMEZONE_PATH = "dataSourceConfig.timeZone";
+
     private OutputCollector collector;
 
     public EnteriseStandardParserBolt(Config config) {
