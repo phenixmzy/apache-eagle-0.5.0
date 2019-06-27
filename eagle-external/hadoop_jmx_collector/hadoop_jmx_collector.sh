49349bc34007ad96162ee86833a6d13478f2d08b
mazhiyong <wingmazhiyong@gmail.com>
2019-06-27 15:01:34 +0800

    fix debug.收集jxm数据

--- eagle-external/hadoop_jmx_collector/hadoop_jmx_collector.sh
+++ eagle-external/hadoop_jmx_collector/hadoop_jmx_collector.sh
@@ -1,2 +1,3 @@
-cd $EAGLE_HOME/lib/scripts
+export EAGLE_HOME=/usr/local/eagle-0.5.0
+cd $EAGLE_HOME/lib/scripts/hadoop_jmx_collector
 /usr/bin/python hadoop_jmx_kafka.py hadoop_jmx_config.json 
--- eagle-external/hadoop_jmx_collector/system_metric_collector.sh
+++ eagle-external/hadoop_jmx_collector/system_metric_collector.sh
@@ -1,2 +1,3 @@
-cd $EAGLE_HOME/lib/scripts
+export EAGLE_HOME=/usr/local/eagle-0.5.0
+cd $EAGLE_HOME/lib/scripts/hadoop_jmx_collector
 /usr/bin/python system_metric_collector.py system_metric_config.json
